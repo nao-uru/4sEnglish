7bdaa8374b63e437752073331d6a22b5b5e7ff39
nao <nao.uru0419@gmail.com>
2022-03-22 14:27:00 +0900

    [add] Smooth Scroll

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const jsSmoothScroll = document.querySelectorAll('a[href^="#"]');
+ 
+for (let i = 0; i < jsSmoothScroll.length; i++){
+  jsSmoothScroll[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    let href = jsSmoothScroll[i].getAttribute('href');
+
+    let target = document.getElementById(href.replace('#', ''));
+
+    const rect = target.getBoundingClientRect().top;
+
+    const offset = window.pageYOffset;
+
+    const position = rect + offset;
+
+    window.scrollTo({
+      top: position,
+      behavior: 'smooth',
+    });
+
+  });
+
+}
+
+
